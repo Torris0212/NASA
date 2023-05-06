cc5bf784606aadd713eb4e0109f16b77347420a1
Torris Jin <torris.jin@grainger.com>
2023-05-06 08:17:08 -0500

    configured package.json

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import morgan from 'morgan';
 
 import planetsRouter from './routes/planets/planets.router.js';
 
@@ -8,7 +10,13 @@ const app = express();
 app.use(cors({
   origin: 'http://localhost:3000'
 }));
+app.use(morgan('combined'));
+app.use(express.static(path.resolve('../server/public')));
 app.use(express.json());
 app.use(planetsRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('../server/public/index.html'));
+});
 
 export default app;
